66ad86028cdeb59442d2eb6a5ab996372e1ec83d
Guilherme Soares <gui.acs26@gmail.com>
2024-05-28 10:43:02 +0100

    projeto TecWeb

--- script/projetos.js
+++ script/projetos.js
@@ -1,8 +1,10 @@
 const listaProjetos = [
     {nome: "birthPlan", link:"projetos/birthPlan/birthPlan.html", descricao:"Este projeto visa melhorar a organização e otimizar os recursos dos serviços de maternidade de um grupo de hospitais.", img: "imagens/projetos/birthPlan/birthPlan.jpeg", id: ["Python"]},
-    //{nome: "Cripter", link:"projetos/Cripter/Cripter.html", descricao:"Ferramenta que ajuda pessoas a encriptar e a desencriptar dados.", img:"imagens/projetos/Cripter/Cripter.jpeg"},
     {nome: "birthPlan 2.0", link:"projetos/birthPlan2/birthPlan2.html", descricao:"Projeto birthPlan mas com melhorias e com objetos.", img:"imagens/projetos/birthPlan2/birthPlan2.jpeg", id: ["Python"]},
+    {nome:"Jogo da Memória", link:"projetos/JogoDaMemoria/JogoDaMemoria.html", descricao: "O Jogo da Memória é um jogo interativo onde os jogadores precisam encontrar pares de cartas iguais. É projetado para ser educativo e divertido, ajudando a melhorar a memória dos jogadores.", img:"imagens/projetos/JogoDaMemoria/JogoDaMemoria.jpg", id: ["Web"]},
+    //{nome: "Cripter", link:"projetos/Cripter/Cripter.html", descricao:"Ferramenta que ajuda pessoas a encriptar e a desencriptar dados.", img:"imagens/projetos/Cripter/Cripter.jpeg"},
+   
     //{nome: "PassSec", link: "projetos/PassSec/PassSec.html", descricao:"Ferramenta para garatir que a tua palavra-passe é segura!", img:"imagens/projetos/PassSec/PassSec.jpeg"},
     //{nome: "IMDB Analyzer", link: "projetos/IMDB_Analyzer/IMDB_Analyzer.html", descricao: "O objetivo do projeto é tornar possivel para todas as pessoas que tenham uma conta IMDB analizarem os seus dados.", img: "projetos/IMDB_Analyzer/IMDB_Analyzer.jpeg"},
-    //{nome:, link:, descricao:, img:},
+    //{nome:, link:, descricao:, img:, id: []},
 ]
