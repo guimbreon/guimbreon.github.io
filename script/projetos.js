13120d90c7213989965d7b6f5356b2ccfb176d92
Guilherme Soares <gui.acs26@gmail.com>
2024-06-22 17:34:41 +0100

    a

--- script/projetos.js
+++ script/projetos.js
@@ -2,6 +2,7 @@ const listaProjetos = [
     {nome: "birthPlan", link:"projetos/birthPlan/birthPlan.html", descricao:"Este projeto visa melhorar a organização e otimizar os recursos dos serviços de maternidade de um grupo de hospitais.", img: "imagens/projetos/birthPlan/birthPlan.jpeg", id: ["Python"]},
     {nome: "birthPlan 2.0", link:"projetos/birthPlan2/birthPlan2.html", descricao:"Projeto birthPlan mas com melhorias e com objetos.", img:"imagens/projetos/birthPlan2/birthPlan2.jpeg", id: ["Python"]},
     {nome:"Jogo da Memória", link:"projetos/JogoDaMemoria/JogoDaMemoria.html", descricao: "O Jogo da Memória é um jogo interativo onde os jogadores precisam encontrar pares de cartas iguais. É projetado para ser educativo e divertido, ajudando a melhorar a memória dos jogadores.", img:"imagens/projetos/JogoDaMemoria/JogoDaMemoria.jpg", id: ["Web"]},
+    {nome:"Média Faculdade", link:"projetos/mediaFaculdade/mediaFaculdade.html", descricao: "Permite calcular a média da Faculdade de uma maneira mais rápida e fácil.", img:"imagens/projetos/mediaFaculdade/mediaFaculdade.jpg", id: ["Web"]},
     //{nome: "Cripter", link:"projetos/Cripter/Cripter.html", descricao:"Ferramenta que ajuda pessoas a encriptar e a desencriptar dados.", img:"imagens/projetos/Cripter/Cripter.jpeg"},
    
     //{nome: "PassSec", link: "projetos/PassSec/PassSec.html", descricao:"Ferramenta para garatir que a tua palavra-passe é segura!", img:"imagens/projetos/PassSec/PassSec.jpeg"},
