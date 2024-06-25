e2634ade0e5a1de3525a64d4f577838c3f560a3f
Guilherme Soares <gui.acs26@gmail.com>
2024-06-25 21:45:14 +0100

    english translation

---
+++ ing/script/projetos.js
@@ -0,0 +1,34 @@
+const listaProjetos = [
+    {
+        nome: "birthPlan", 
+        link: "projetos/birthPlan/birthPlan.html", 
+        descricao: "This project aims to improve the organization and optimize the resources of the maternity services of a group of hospitals.", 
+        img: "imagens/projetos/birthPlan/birthPlan.jpeg", 
+        id: ["Python"]
+    },
+    {
+        nome: "birthPlan 2.0", 
+        link: "projetos/birthPlan2/birthPlan2.html", 
+        descricao: "BirthPlan project with improvements and objects.", 
+        img: "imagens/projetos/birthPlan2/birthPlan2.jpeg", 
+        id: ["Python"]
+    },
+    {
+        nome: "Memory Game", 
+        link: "projetos/JogoDaMemoria/JogoDaMemoria.html", 
+        descricao: "The Memory Game is an interactive game where players need to find pairs of matching cards. It is designed to be educational and fun, helping to improve players' memory.", 
+        img: "imagens/projetos/JogoDaMemoria/JogoDaMemoria.jpg", 
+        id: ["Web"]
+    },
+    {
+        nome: "College Average", 
+        link: "projetos/mediaFaculdade/mediaFaculdade.html", 
+        descricao: "Allows calculating the college average in a faster and easier way.", 
+        img: "imagens/projetos/mediaFaculdade/mediaFaculdade.jpg", 
+        id: ["Web"]
+    },
+    // {nome: "Cripter", link: "projetos/Cripter/Cripter.html", descricao: "Tool that helps people encrypt and decrypt data.", img: "imagens/projetos/Cripter/Cripter.jpeg"},
+    // {nome: "PassSec", link: "projetos/PassSec/PassSec.html", descricao: "Tool to ensure your password is secure!", img: "imagens/projetos/PassSec/PassSec.jpeg"},
+    // {nome: "IMDB Analyzer", link: "projetos/IMDB_Analyzer/IMDB_Analyzer.html", descricao: "The goal of the project is to make it possible for everyone with an IMDB account to analyze their data.", img: "projetos/IMDB_Analyzer/IMDB_Analyzer.jpeg"},
+    // {nome: , link: , descricao: , img: , id: []},
+]
---
+++ ing/script/projetosLoader.js
@@ -0,0 +1,86 @@
+function escolherProjetos(projetos){
+    const projetosBaralhados = projetos.sort(() => Math.random() - 0.2);
+
+    const projetosEscolhidos = projetosBaralhados.slice(0, 2);
+    return projetosEscolhidos;
+}
+
+
+function mostrarProjetos(projetos){
+    projetos = escolherProjetos(projetos);
+    for (let i = 0; i < listaProjetos.length; i++) {
+        let projeto = projetos[i];
+        const divPost = document.createElement("div");
+        const divPostImg = document.createElement("div");
+        const divPostCont = document.createElement("div");
+        const where = document.getElementById("here");
+        const img = document.createElement("img");
+        img.src = projeto.img;
+        img.alt = "imagem do projeto " + projeto.nome
+        divPostImg.appendChild(img);
+        
+
+        const h2 = document.createElement("h2")
+        const p = document.createElement("p")
+        const a = document.createElement("a")
+
+        h2.innerHTML = projeto.nome
+        
+        a.innerHTML = "Read more"
+        a.href = projeto.link
+        p.innerHTML = projeto.descricao
+
+        divPost.className = "post";
+        divPostImg.className = "post-image";
+        divPostCont.className = "post-content";
+
+        divPostCont.appendChild(h2);
+        divPostCont.appendChild(p);
+        divPostCont.appendChild(a);
+
+
+        divPost.appendChild(divPostImg);
+        divPost.appendChild(divPostCont);
+        where.appendChild(divPost);
+    }
+}
+
+
+function mostrarProjetos2(projetos){
+    for (let i = 0; i < listaProjetos.length; i++) {
+        let projeto = projetos[i];
+        const divPost = document.createElement("div");
+        const divPostImg = document.createElement("div");
+        const divPostCont = document.createElement("div");
+        const where = document.getElementById("here");
+        const img = document.createElement("img");
+
+        divPost.id = projeto.id
+        img.src = "../" + projeto.img;
+        img.alt = "imagem do projeto " + projeto.nome
+        divPostImg.appendChild(img);
+        
+
+        const h2 = document.createElement("h2")
+        const p = document.createElement("p")
+        const a = document.createElement("a")
+
+        h2.innerHTML = projeto.nome
+        a.innerHTML = "Read more"
+        a.href = "../" + projeto.link
+        p.innerHTML = projeto.descricao
+
+        divPost.className = "post";
+        divPostImg.className = "post-image";
+        divPostCont.className = "post-content";
+
+        divPostCont.appendChild(h2);
+        divPostCont.appendChild(p);
+        divPostCont.appendChild(a);
+
+
+        divPost.appendChild(divPostImg);
+        divPost.appendChild(divPostCont);
+        where.appendChild(divPost);
+    }
+}
