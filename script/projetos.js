094d361b10ba51f408c72d72df70e06dc1022e3b
Guilherme Soares <gui.acs26@gmail.com>
2024-12-13 23:33:43 +0000

    a

--- script/projetos.js
+++ script/projetos.js
@@ -4,6 +4,7 @@ const listaProjetos = [
     {nome:"Jogo da Memória", link:"projetos/JogoDaMemoria/JogoDaMemoria.html", descricao: "O Jogo da Memória é um jogo interativo onde os jogadores precisam encontrar pares de cartas iguais. É projetado para ser educativo e divertido, ajudando a melhorar a memória dos jogadores.", img:"imagens/projetos/JogoDaMemoria/JogoDaMemoria.jpg", id: ["Web"]},
     {nome:"Média Faculdade", link:"projetos/mediaFaculdade/mediaFaculdade.html", descricao: "Permite calcular a média da Faculdade de uma maneira mais rápida e fácil.", img:"imagens/projetos/mediaFaculdade/mediaFaculdade.jpg", id: ["Web"]},
     {nome:"safeLevadas", link:"projetos/safeLevadas/safeLevadas.html", descricao: "Destinada a encontrar os percursos mais rápidos entre dois pontos na rede de levadas da ilha da Madeira, melhorando o suporte e resgate de caminhantes.", img:"imagens/projetos/safeLevadas/safeLevadas.jpeg", id: ["Python"]},
+    {nome:"memento Gallery", link:"projetos/mementoGallery/mementoGallery.html", descricao: "Galeria de fotografias.", img:"imagens/projetos/mementoGallery/mementoGallery.png", id: ["Web"]},
     //{nome: "Cripter", link:"projetos/Cripter/Cripter.html", descricao:"Ferramenta que ajuda pessoas a encriptar e a desencriptar dados.", img:"imagens/projetos/Cripter/Cripter.jpeg"},
    
     //{nome: "PassSec", link: "projetos/PassSec/PassSec.html", descricao:"Ferramenta para garatir que a tua palavra-passe é segura!", img:"imagens/projetos/PassSec/PassSec.jpeg"},
